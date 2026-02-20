19ac8764ef1b484652dfc291292672285f579bd0
Ada Shi <adashiyj@gmail.com>
2026-02-20 15:48:26 +0100

    Enhance NAOqi import with text-to-speech functionality

--- code/test_naoqi.py
+++ code/test_naoqi.py
@@ -1,2 +1,9 @@
 import naoqi
-print("NAOqi imported successfully!")
+from naoqi import ALProxy
+
+ROBOT_IP = "192.168.0.102"
+PORT = 9559
+
+tts = ALProxy("ALTextToSpeech", ROBOT_IP, PORT)
+tts.say("Hello, I am connected!")
+print("Connection successful!")
