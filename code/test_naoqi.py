1ae3352efab0420908a268cfd8ae082836cd214b
Ada Shi <adashiyj@gmail.com>
2026-02-20 15:50:21 +0100

    Update test_naoqi.py

--- code/test_naoqi.py
+++ code/test_naoqi.py
@@ -1,9 +1,2 @@
 import naoqi
-from naoqi import ALProxy
-
-ROBOT_IP = "192.168.0.102"
-PORT = 9559
-
-tts = ALProxy("ALTextToSpeech", ROBOT_IP, PORT)
-tts.say("Hello, I am connected!")
-print("Connection successful!")
+print("NAOqi imported successfully!")
